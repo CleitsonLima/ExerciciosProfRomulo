b3f48c275138ce6be053e0d87fe7146e3d2acf44
CleitsonLima <cleitson.ferreira@aluno.senai.br>
2022-07-06 11:42:23 -0300

    exercicio c#

---
+++ ExerciciosProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            do
+            {
+                double num1, num2, num3, soma, media;
+                string nome;
+
+
+                Console.WriteLine("\nDigite seu nome");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("digite a primeira nota");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite a segunda nota");
+                num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite a terceira nota");
+                num3 = double.Parse(Console.ReadLine());
+
+                soma = num1 + num2 + num3;
+                media = (num1 + num2 + num3) / 3;
+
+
+
+                //Console.WriteLine("O aluno " + nome + " tirou as seguintes notas: " +num1 + "," + num2 + "," + num3+ " totalizando " + soma );
+
+                Console.WriteLine($"O aluno {nome} tirou as seguintes notas: {num1}, {num2}, {num3}, totalizando {soma}");
+                Console.WriteLine($"A media final do aluno e igual a {media.ToString("F")}.");
+
+                if (media >= 7)
+                {
+                    Console.WriteLine("Aprovado");
+                }
+                else
+                {
+                    Console.WriteLine("Reprovado");
+                }
+
+                Console.WriteLine("Para continuar digite qualquer tecla diferente de barra de espaco");
+                //sair = int.Parse(Console.ReadLine());
+
+                
+            } while (Console.ReadKey().Key != ConsoleKey.Spacebar );
+            
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2,soma;
+            
+
+            Console.WriteLine("digite um numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite outro  numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+            Console.WriteLine($"A soma entre os numeros {num1} e {num2} e igual a {soma}.");
+            Console.ReadKey();
+        }
+        
+    }
+}
